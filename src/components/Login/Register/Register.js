ab2ffcc35d4139a42de1f1c4ba850b807918b02c
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-05-02 13:10:34 +0600

    implemented login and register form

--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 import './Register.css';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import Loading from '../../Shared/Loading/Loading';
+
 
 const Register = () => {
+    const [agree, setAgree] = useState(false);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+
+    const navigate = useNavigate();
+
+        const navigateLogin = () =>{
+        navigate('/login');
+    }
+
+    if(loading || updating){
+        return <Loading></Loading>
+    }
+
+    const handleRegister = async(event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name});
+          alert('Updated profile');
+          navigate('/home')
+    }
     return (
         <div className='register-form'>
-             <h2 style={{textAlign: 'center'}}>Please Register</h2>
-             <form>
-             <input type="text" name="name" id="" placeholder='YourName'/>
+            <h2 style={{textAlign: 'center'}}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='YourName'/>
                 <input type="email" name="email" id="" placeholder='Your Email Address' required/>
                 <input type="password" name="password" id="" placeholder='Password' required/> 
+               <input onClick={()=> setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept Genius Car Terms and Condition</label>
                 <input
+                 disabled={!agree}
                  className='btn btn-primary w-50 mx-auto mt-2' type="submit" value="Register" />
-             </form>
+            </form>
+            <p>Already have an account? <Link to="/login" className="text-primary pe-auto text-decoration-none" onClick={navigateLogin}>Please Login</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,9 +1,18 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import './Header.css';
 
 const Header = () => {
+
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  }
     return (
         <div>
  <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
@@ -15,7 +24,11 @@ const Header = () => {
       <Nav.Link as={Link} to="home">Home</Nav.Link>
       <Nav.Link as={Link} to="inventory">Inventory</Nav.Link>
       <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
-      <Nav.Link as={Link} to="login">Login</Nav.Link>
+      {
+        user ?
+        <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Sign out</button>
+        :
+        <Nav.Link as={Link} to="login">Login</Nav.Link>}
     </Nav>
   </Navbar.Collapse>
   </Container>
